7b9a33a21d72567488762120d36d25437255b8c1
andredw13L <andreribeiro13l@gmail.com>
2024-06-22 00:42:53 -0300

    Completar lista

--- src/App.jsx
+++ src/App.jsx
@@ -41,9 +41,8 @@ function App() {
 
 
   const removeTodo = (id) => {
-    
-    const newTodos = [...todos]
 
+    const newTodos = [...todos]
     const filteredTodos = newTodos.filter(
       todo => todo.id !== id ? todo : null
     )
@@ -51,12 +50,26 @@ function App() {
     setTodos(filteredTodos)
   }
 
+  const completeTodo = (id) => {
+    
+    const newTodos = [...todos]
+    newTodos.map(
+      (todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo
+    )
+
+    setTodos(newTodos)
+  }
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo}/>
+          <Todo 
+            key={todo.id} todo={todo} 
+            removeTodo={removeTodo} 
+            completeTodo={completeTodo}
+          />
         ))}
       </div>
       <TodoForm addTodo={addTodo}/>
